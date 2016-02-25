5b13e9c82563e7277c63e633813682d4865510d4
jumal <jumal@apache.org>
2016-02-25 19:39:25 +0000

    Initial commit

--- servers/build/create.sh
+++ servers/build/create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 DIRECTORY=$(cd ${0%/*} && echo $PWD)
 LIB=$DIRECTORY/../../lib
+REGISTRY=`$LIB/get-registry.sh`
 
 $DIRECTORY/image/create.sh
 
@@ -8,4 +9,5 @@ $LIB/create-key-pair.sh build-server
 ecs-cli configure --region `$LIB/get-region.sh` --cluster build-server
 ecs-cli up --instance-type t2.micro --size 1 --keypair build-server --capability-iam
 aws iam attach-role-policy --role-name `$LIB/get-role.sh build-server` --policy-arn arn:aws:iam::aws:policy/AWSCodePipelineCustomActionAccess
+sed -i '' -e "s/REGISTRY/$REGISTRY/g" $DIRECTORY/docker-compose.yml
 ecs-cli compose --file $DIRECTORY/docker-compose.yml up
