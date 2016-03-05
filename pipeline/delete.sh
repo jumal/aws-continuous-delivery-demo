0da3c2947e9d92a9ea6c99e56a57eb935c06770a
jumal <jumal@apache.org>
2016-03-05 18:16:59 +0000

    Initial commit

--- pipeline/delete.sh
+++ pipeline/delete.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -ex
-aws --region us-east-1 cloudformation delete-stack --stack-name project
+BUCKET=$(aws --region us-east-1 cloudformation describe-stack-resource --stack-name project-pipeline --logical-resource-id artifactStore --output text --query 'StackResourceDetail.PhysicalResourceId')
+aws s3 rm s3://$BUCKET --recursive
+aws --region us-east-1 cloudformation delete-stack --stack-name project-pipeline
