a1bd3a3186181cc6e7c9200f2f4b435d0f7f0daf
jumal <jumal@apache.org>
2016-03-10 22:54:00 +0000

    Initial commit

--- servers/project-qa/delete.sh
+++ servers/project-qa/delete.sh
@@ -4,6 +4,6 @@ LIB=$DIRECTORY/../../lib
 CLUSTER=$(basename $DIRECTORY)
 
 ecs-cli configure --region $(aws configure get region) --cluster $CLUSTER
-ecs-cli compose --file $DIRECTORY/docker-compose.yml service rm
+ecs-cli compose service rm
 ecs-cli down --force
 $LIB/delete-key-pair.sh $CLUSTER
