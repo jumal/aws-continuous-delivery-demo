c775f0bb11509cbfffae4ef0c75b824821cd6f01
jumal <jumal@apache.org>
2016-03-11 22:05:56 +0000

    Initial commit

--- servers/project-build-server/create.sh
+++ servers/project-build-server/create.sh
@@ -8,7 +8,7 @@ $DIRECTORY/sonarqube/create.sh
 
 $LIB/create-key-pair.sh $CLUSTER
 ecs-cli configure --region $(aws configure get region) --cluster $CLUSTER
-ecs-cli up --instance-type t2.small --size 1 --keypair $CLUSTER --capability-iam
+ecs-cli up --instance-type t2.medium --keypair $CLUSTER --capability-iam
 ROLE=$($LIB/get-role.sh $CLUSTER)
 aws iam attach-role-policy --role-name $ROLE --policy-arn arn:aws:iam::aws:policy/AWSCodePipelineCustomActionAccess
 aws iam attach-role-policy --role-name $ROLE --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser
