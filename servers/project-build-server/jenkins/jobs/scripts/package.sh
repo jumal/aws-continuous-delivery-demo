cc6b4372ddcd73a9ea4eb6ee3f5e6b49ab55f8bc
jumal <jumal@apache.org>
2016-03-11 23:40:18 +0000

    Initial commit

--- servers/project-build-server/jenkins/jobs/scripts/package.sh
+++ servers/project-build-server/jenkins/jobs/scripts/package.sh
@@ -4,7 +4,7 @@ LOGIN=$(aws --region us-east-1 ecr get-login)
 REGISTRY=$(echo $LOGIN | awk '{print $9}' | sed 's/^https:\/\///g')
 
 docker build -t project:$TAG .
-docker tag project:$TAG $REGISTRY/project:$TAG
+docker tag -f project:$TAG $REGISTRY/project:$TAG
 $LOGIN
 docker push $REGISTRY/project:$TAG
 sed -i -e "s/REGISTRY/$REGISTRY/g" -e "s/TAG/$TAG/g" docker-compose.yml
