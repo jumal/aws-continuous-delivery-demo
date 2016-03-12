1382bb7c5f0139dc2aca7875167b5e39b6b67fda
jumal <jumal@apache.org>
2016-03-12 09:19:41 +0000

    Initial commit

--- servers/project-build-server/jenkins/jobs/scripts/deploy.sh
+++ servers/project-build-server/jenkins/jobs/scripts/deploy.sh
@@ -3,5 +3,6 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: deploy.sh CLUSTER"
     exit 1
 fi
-ecs-cli configure --region $(aws configure get region) --cluster $1
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | awk -F\" '/region/ {print $4}')
+ecs-cli configure --region $REGION --cluster $1
 ecs-cli compose --project-name $1 service up
