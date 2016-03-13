4b251f90e4085da8051089cad310b37091e0edc2
jumal <jumal@apache.org>
2016-03-13 03:04:23 +0000

    Initial commit

--- servers/project-build-server/policies.sh
+++ servers/project-build-server/policies.sh
@@ -3,6 +3,7 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: policies.sh attach|detach"
     exit 1
 fi
+DIRECTORY=$(cd ${0%/*} && echo $PWD)
 CLUSTER=$(basename $DIRECTORY)
 ROLE=$(aws cloudformation describe-stack-resource --stack-name amazon-ecs-cli-setup-$CLUSTER --logical-resource-id EcsInstancePolicy --output text --query 'StackResourceDetail.PhysicalResourceId')
 for policy in AWSCodePipelineCustomActionAccess AmazonEC2ContainerRegistryPowerUser AmazonEC2ContainerServiceFullAccess; do
